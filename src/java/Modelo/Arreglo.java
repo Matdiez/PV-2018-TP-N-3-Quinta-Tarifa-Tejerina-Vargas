13d822f551647d6473cc1e70735b1a490c6aac11
Marcos <Marcos@LAPTOP-A9H74192>
2018-04-28 19:17:03 -0300

    Resolución del punto 1 (promedio y mayor/menor)

--- src/java/Modelo/Arreglo.java
+++ src/java/Modelo/Arreglo.java
@@ -20,6 +20,29 @@ public class Arreglo {
         }
     }
 
+        
+    public void mostrarNum() {
+        int num = getArreglo().length;
+        double prom = 0;
+        int i, j, may, men;
+        for(i=0;i<10;i++) {
+            System.out.println(getArreglo()[i]);
+            prom = prom+getArreglo()[i];
+        }
+        prom = prom/10;
+        may=men=arreglo[0];
+        for(j=1;j<10;j++) {
+            if(arreglo[j]>may) {
+                may = arreglo[j];
+            }
+            if(arreglo[j]<men) {
+                men = arreglo[j];
+            }
+        }
+        System.out.println("El n° mayor es: " + may);
+        System.out.println("El n° menor es: " + men);
+        System.out.println("El promedio es: " + prom);
+    }
     /**
      * @return the arreglo
      */
